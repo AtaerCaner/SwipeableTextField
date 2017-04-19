1207d5f438528fcc54efbf53bdb599aa4e5bedbe
Caner CELİK <ataercanercelik@gmail.com>
2017-04-19 13:52:16 +0300

    Initial Commit

---
+++ SwipeableTextField/SwipeableTextField.h
@@ -0,0 +1,19 @@
+//
+//  SwipeableTextField.h
+//  SwipeableTextField
+//
+//  Created by Ataer Caner on 19/04/17.
+//  Copyright © 2017 Bckspace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwipeableTextField.
+FOUNDATION_EXPORT double SwipeableTextFieldVersionNumber;
+
+//! Project version string for SwipeableTextField.
+FOUNDATION_EXPORT const unsigned char SwipeableTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwipeableTextField/PublicHeader.h>
+
+
